167e062c1725800deedfb7ab478ce3c8227db9f7
Marcela Estrada <57819286+marcelaestrada@users.noreply.github.com>
2022-05-30 16:47:25 -0600

    folder watcher, has some bugs

---
+++ Programming Exercise/FolderWatcher/FolderWatcher/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace FolderWatcher
+{
+    partial class FolderWatcherForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblChanges = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Location = new System.Drawing.Point(31, 31);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(130, 15);
+            this.lblSelect.TabIndex = 0;
+            this.lblSelect.Text = "Select a folder to watch";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(31, 49);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(426, 23);
+            this.tbPath.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(465, 48);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(77, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "...";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblChanges
+            // 
+            this.lblChanges.AutoSize = true;
+            this.lblChanges.Location = new System.Drawing.Point(18, 29);
+            this.lblChanges.Name = "lblChanges";
+            this.lblChanges.Size = new System.Drawing.Size(54, 15);
+            this.lblChanges.TabIndex = 3;
+            this.lblChanges.Text = "Cambios";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblChanges);
+            this.groupBox1.Location = new System.Drawing.Point(31, 92);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(511, 234);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // FolderWatcherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 351);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.lblSelect);
+            this.Name = "FolderWatcherForm";
+            this.RightToLeftLayout = true;
+            this.Text = "Folder Watcher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblChanges;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Programming Exercise/FolderWatcher/FolderWatcher/Form1.cs
@@ -0,0 +1,199 @@
+﻿using Spire.Xls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderWatcher
+{
+    public partial class FolderWatcherForm : Form
+    {
+        string masterPath = "";
+        string targetPath = "";
+        string targetExcel = "";
+
+        public FolderWatcherForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            folderBrowser.ShowDialog();
+            string path = folderBrowser.SelectedPath;
+
+            if (path != "") {
+                tbPath.Text = path;
+                tbPath.Enabled = false;
+                folderWatcher(path);
+            }
+        }
+
+        private void folderWatcher (string path)
+        {
+            FileSystemWatcher watch = new FileSystemWatcher();
+            watch.Path = path;
+            watch.IncludeSubdirectories = false;
+
+            watch.NotifyFilter = NotifyFilters.Attributes |
+                NotifyFilters.CreationTime |
+                NotifyFilters.DirectoryName |
+                NotifyFilters.FileName |
+                NotifyFilters.LastAccess |
+                NotifyFilters.LastWrite |
+                NotifyFilters.Security |
+                NotifyFilters.Size;
+
+            watch.Filter = "*.*";
+
+            watch.Created += new FileSystemEventHandler(createdFile);
+            watch.Changed += new FileSystemEventHandler(moveFile);
+            watch.EnableRaisingEvents = true;
+
+            // First in 
+            DirectoryInfo dInfo = new DirectoryInfo(path);
+
+            if (!Directory.Exists(dInfo.FullName + "\\Processed"))
+            {
+                dInfo.CreateSubdirectory("Processed");
+            }
+
+            if (!Directory.Exists(dInfo.FullName + "\\Not applicable"))
+            {
+                dInfo.CreateSubdirectory("Not applicable");
+            }
+
+            if (!File.Exists(dInfo.FullName + "\\master.xlsx"))
+            {
+                File.Create(dInfo.FullName + "\\master.xlsx");
+            }
+            masterPath = dInfo.FullName + "\\master.xlsx";
+            targetPath = dInfo.FullName + "\\Not applicable\\";
+            targetExcel = dInfo.FullName + "\\Processed\\";
+
+            FileInfo[] files = dInfo.GetFiles();
+
+            if(files.Length != 0)
+            {
+                foreach (FileInfo f in files)
+                {
+                    if (f.Extension == ".xlsx" && f.Name != "master.xlsx")
+                    {
+                        // Check if it is excel or not, save the worksheets into a general worksheet
+                        isExcel(f.Name, f.FullName, masterPath);
+
+
+                        try
+                        {
+                            // Move file to another directory inside the folder
+                            File.Move(f.FullName, targetExcel + f.Name);
+                            lblChanges.Text = f.Name + " was copied to " + masterPath + " and then moved to " + targetExcel;
+                            //addGroupBox(f.Name + " was copied to " + masterPath + " and then moved to " + targetExcel);
+                        }
+                        catch
+                        {
+                            //addGroupBox(f.Name + " was copied to " + masterPath + " but it can not be moved since it is open");
+                        }
+                    }
+                    else if (f.Name != "master.xlsx")
+                    {
+                        try
+                        {
+                            File.Move(f.FullName, targetPath + f.Name);
+                            //addGroupBox(f.Name + " was moved to " + targetPath);
+                        }
+                        catch
+                        {
+                            //addGroupBox(f.Name + " can not be moved to " + targetPath + " since it is open. ");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void createdFile(object source, FileSystemEventArgs e)
+        {
+            // Check if it is excel or not, save the worksheets into a general worksheet
+            if (e.Name.Contains(".xlsx") && e.Name != "master.xlsx")
+            {
+                isExcel(e.Name, e.FullPath, masterPath);
+
+                // Move file to another directory inside the folder
+                try
+                {
+                    File.Move(e.FullPath, targetExcel + e.Name);
+                    //addGroupBox(e.Name + " was copied to " + masterPath + " and then moved to " + targetPath);
+                }
+                catch
+                {
+                    //addGroupBox(e.Name + " was copied to " + masterPath + " but it can not be moved since it is open");
+                }
+                
+            }
+            else if (e.Name != "master.xlsx")
+            {
+                int milliseconds = 2000;
+                Thread.Sleep(milliseconds);
+
+                try
+                {
+                    File.Move(e.FullPath, targetPath + e.Name);
+                    //addGroupBox(e.Name + " was moved to " + targetPath);
+                }
+                catch
+                {
+                    //addGroupBox(e.Name + " can not be moved to " + targetPath +  " since it is open. ");
+                }
+            }
+
+        }
+
+        private void moveFile(object source, FileSystemEventArgs e)
+        {
+            try
+            {
+                File.Move(e.FullPath, targetPath + e.Name);
+                //addGroupBox(e.Name + " was moved to " + targetPath);
+            }
+            catch
+            {
+                //addGroupBox(e.Name + " can not be moved to " + targetPath + " since it is open. ");
+            }
+        }
+
+        private void isExcel(string name, string filepath, string masterPath)
+        {
+            int milliseconds = 2000;
+            Thread.Sleep(milliseconds);
+
+            // Source excel
+            Workbook source = new Workbook();
+            source.LoadFromFile(filepath);
+
+            // Target excel - master
+            Workbook target = new Workbook();
+            target.LoadFromFile(masterPath);
+
+            // Copy and save worksheets as "filename - sheet name"
+            foreach (Worksheet w in source.Worksheets)
+            {
+                Worksheet copyS = target.Worksheets.AddCopy(w);
+                copyS.Name = name + " - " + w.Name + " " + target.Worksheets.Count;
+                target.SaveToFile(masterPath);
+            }
+        }
+
+        //private void addGroupBox(string text)
+        //{
+        //    gbCambios.Text += "\n" + text;
+        //}
+
+    }
+}
